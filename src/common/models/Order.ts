e77892cda69ffd023f4f4bdc87a37cf6ae925d21
boxfox619 <boxfoxsg619@gmail.com>
2023-05-14 22:26:47 +0900

    fid order id type

--- src/common/models/Order.ts
+++ src/common/models/Order.ts
@@ -1,9 +1,9 @@
-import { Address } from "./Address";
-import { OrderStatus } from "./OrderStatus";
-import { Product, ProductOption } from "./Product";
+import { Address } from './Address';
+import { OrderStatus } from './OrderStatus';
+import { Product, ProductOption } from './Product';
 
 export interface Order {
-  id: string;
+  id: number;
   cost: number;
   time: Date;
   address: Address;
