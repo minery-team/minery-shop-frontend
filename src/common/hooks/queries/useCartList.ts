003c4c25a27700cb81c6091be64fff3e50fe1a2d
HC-Gwon <gurckddlz@naver.com>
2023-05-20 01:29:31 +0900

    fix: useCartList 훅에서 테스트 데이터 던져주도록 수정

--- src/common/hooks/queries/useCartList.ts
+++ src/common/hooks/queries/useCartList.ts
@@ -2,10 +2,44 @@ import { useQuery } from '@tanstack/react-query';
 
 import { fetchCartList } from '@/common/api/cart';
 
+export const TEST_CART_DATA = [
+  {
+    id: 1,
+    amount: 1,
+    product: {
+      id: 1,
+      image: '',
+      name: 'wine1',
+      price: 10000,
+    },
+  },
+  {
+    id: 2,
+    amount: 3,
+    product: {
+      id: 2,
+      image: '',
+      name: 'wine2',
+      price: 20000,
+    },
+  },
+  {
+    id: 3,
+    amount: 1,
+    product: {
+      id: 3,
+      image: '',
+      name: 'wine3',
+      price: 30000,
+    },
+  },
+];
+
 export const useCartList = () => {
   const query = useQuery(['/cart'], async () => {
     try {
-      return await fetchCartList();
+      return TEST_CART_DATA;
+      // return await fetchCartList();
     } catch {
       return undefined;
     }
