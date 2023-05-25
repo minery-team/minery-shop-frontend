90e937bda75efda655a455c0ecd6b6d266a53bec
HC-Gwon <gurckddlz@naver.com>
2023-05-26 01:10:26 +0900

    fix: index 페이지 수정 - useQuery -> useCartList - useState -> 필요한 useQuery 사용

--- src/cart/pages/index.tsx
+++ src/cart/pages/index.tsx
@@ -1,33 +1,15 @@
-import { useState, useRef } from 'react';
 import { useRouter } from 'next/router';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import styled from '@emotion/styled';
 import { Icon } from '@iconify/react';
 
 import WineList from '@cart/pages/wineList';
 import NoWineList from '@cart/pages/noWineList';
-import { CartItem } from '@/common/models';
+import { useCartList } from '@/common/hooks/queries/useCartList';
+import { clearCart } from '@/common/api/cart';
 
 export default function CartPage() {
   const router = useRouter();
-  const [wineList, setWineList] = useState<CartItem[]>([]);
-
-  useQuery(
-    ['cart-list'],
-    async () => {
-      return await axios.get('http://localhost:3000/api/cart');
-    },
-    {
-      onSuccess: (res) => {
-        console.log('success res : ', res);
-        setWineList(res.data.item);
-      },
-      onError: (err) => {
-        console.log('error : ', err);
-      },
-    }
-  );
+  const [cartList, refetch] = useCartList();
 
   return (
     <Container>
@@ -46,15 +28,13 @@ export default function CartPage() {
           width={30}
           height={30}
           onClick={() => {
-            setWineList([]);
+            clearCart();
+            refetch();
           }}
         />
       </TopNavigator>
-      {wineList.length ? (
-        <WineList
-          wineList={wineList}
-          setWineList={(wineList: CartItem[]) => setWineList(wineList)}
-        />
+      {cartList && cartList.length ? (
+        <WineList wineList={cartList} />
       ) : (
         <NoWineList />
       )}
