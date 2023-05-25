027f806a2bb555c06653ef241afcadb1c1b2a093
HC-Gwon <gurckddlz@naver.com>
2023-05-26 01:09:42 +0900

    fix: wineList 코드 수정 - windList 렌더링 하는 부분을 WineListItem 컴포넌트 구현으로 간략하게 구현 - totalPrice를 lodash 사용으로 간략하게 구현

--- src/cart/pages/wineList.tsx
+++ src/cart/pages/wineList.tsx
@@ -1,93 +1,23 @@
 import { useMemo } from 'react';
 import { useRouter } from 'next/router';
-import Image from 'next/image';
-import { Icon } from '@iconify/react';
 import styled from '@emotion/styled';
 import { commaizeNumber } from '@boxfox/utils';
+import { sum } from 'lodash';
 
-import { addToCart, deleteFromCart, clearCart } from '@/common/api/cart';
+import WineListItem from '@cart/components/WineListItem';
 import { CartItem } from '@/common/models';
 
-interface Props {
-  wineList: CartItem[];
-  setWineList: Function;
-}
-
-export default function WineList({ wineList, setWineList }: Props) {
+export default function WineList({ wineList }: { wineList: CartItem[] }) {
   const router = useRouter();
 
-  const totalPrice = useMemo(() => {
-    let tmp = 0;
-
-    wineList.map((item: CartItem) => {
-      tmp += item.amount * item.product.price;
-    });
-
-    return tmp;
-  }, [wineList]);
+  const totalPrice = useMemo(
+    () => sum(wineList.map((item) => item.amount * item.product.price)),
+    [wineList]
+  );
 
   const renderWineList = () => {
     return wineList.map((item: CartItem, index: number) => {
-      return (
-        <ItemWrapper key={`${item.product.name}_${index}`}>
-          <div>
-            <Image
-              src={item.product.image}
-              alt={`${item.id}`}
-              width={50}
-              height={50}
-            />
-            <div>{item.product.name}</div>
-            <div>{commaizeNumber(item.product.price)}원</div>
-          </div>
-          <div>
-            <Icon
-              icon="material-symbols:close"
-              width={30}
-              height={30}
-              onClick={() => {
-                const tmp = [...wineList];
-
-                tmp.splice(index, 1);
-                setWineList([...tmp]);
-
-                // clearCart();
-              }}
-            />
-            <CountWrapper>
-              <Icon
-                icon="entypo:circle-with-minus"
-                width={30}
-                height={30}
-                onClick={() => {
-                  const tmp = [...wineList];
-
-                  if (tmp[index].amount > 1) {
-                    tmp[index].amount -= 1;
-                    setWineList([...tmp]);
-
-                    // deleteFromCart();
-                  }
-                }}
-              />
-              <div>{item.amount}</div>
-              <Icon
-                icon="entypo:circle-with-plus"
-                width={30}
-                height={30}
-                onClick={() => {
-                  const tmp = [...wineList];
-
-                  tmp[index].amount += 1;
-                  setWineList([...tmp]);
-
-                  // addToCart();
-                }}
-              />
-            </CountWrapper>
-          </div>
-        </ItemWrapper>
-      );
+      return <WineListItem item={item} index={index} />;
     });
   };
 
@@ -104,20 +34,6 @@ export default function WineList({ wineList, setWineList }: Props) {
     </>
   );
 }
-
-const ItemWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100vw;
-  height: 200px;
-  padding: 0 20px;
-  background-color: pink;
-  border: 1px solid black;
-`;
-
-const CountWrapper = styled.div`
-  display: flex;
-`;
 
 const BottomNavigator = styled.div`
   position: fixed;
