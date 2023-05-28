5f5a472fc226842143b4ef3fc1c3c749f4ae87c0
boxfox619 <boxfoxsg619@gmail.com>
2023-05-28 19:30:45 +0900

    dummy code 복구

--- src/common/api/auth/fetchCurrentUser.ts
+++ src/common/api/auth/fetchCurrentUser.ts
@@ -1,11 +1,12 @@
 import { assertAxiosError } from '@boxfoxs/axios';
 import { User } from '@/common/models/User';
-import { requester } from '@/common/api/requester';
 
 export async function fetchCurrentUser() {
   try {
-    const res = await requester.get<User>('/auth/me');
-    return res.data;
+    // const res = await requester.get<User>('/auth/me');
+    // return res.data;
+    return dummyUser;
+    // return undefined;
   } catch (e) {
     assertAxiosError(e);
     if (e.response?.status === 401) {
@@ -14,3 +15,12 @@ export async function fetchCurrentUser() {
     throw e;
   }
 }
+
+// TODO: Remove
+const dummyUser: User = {
+  id: 0,
+  name: '알콜과 엔돌핀',
+  email: 'drink@drink.com',
+  phone: '010-2023-0514',
+  registeredAt: '2021-01-01',
+};
