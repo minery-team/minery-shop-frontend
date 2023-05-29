0b3b7a395d53166334a7bc03d98cd446fa134354
boxfox619 <boxfoxsg619@gmail.com>
2023-05-29 18:03:05 +0900

    InputField 기본 스타일 구현, auth 페이지 적용

--- src/common/components/form/InputField.tsx
+++ src/common/components/form/InputField.tsx
@@ -1,8 +1,52 @@
-import React, { ComponentProps, ForwardedRef } from 'react';
+import { colors } from '@/common/constants';
+import { useTextStyle } from '@boxfoxs/bds-web';
+import styled from '@emotion/styled';
+import React, {
+  CSSProperties,
+  ComponentProps,
+  ForwardedRef,
+  ReactNode,
+} from 'react';
+
+interface Props extends ComponentProps<'input'> {
+  label?: string;
+  right?: ReactNode;
+  containerStyle?: CSSProperties;
+}
 
 export const InputField = React.forwardRef(function InputField(
-  props: ComponentProps<'input'>,
+  { label, right, style, containerStyle, ...props }: Props,
   ref: ForwardedRef<HTMLInputElement>
 ) {
-  return <input {...props} ref={ref} />;
+  const textStyle = useTextStyle({ size: 'xl' });
+  return (
+    <Container>
+      {label && <Label>{label}</Label>}
+      <StyledInput {...props} ref={ref} style={{ ...textStyle, ...style }} />
+      {right}
+    </Container>
+  );
 });
+
+const Container = styled.div``;
+
+const Label = styled.label`
+  color: ${colors.gray600};
+  margin-bottom: 4px;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 8px 0;
+  color: ${colors.gray900};
+  border: none;
+  outline: none;
+  border-bottom: 1px solid ${colors.gray300};
+  &:focus {
+    border-color: ${colors.primary700Default};
+  }
+  transition: all 150ms;
+  &::placeholder {
+    color: ${colors.gray400};
+  }
+`;
