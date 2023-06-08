bc5cafabeb7d4a685fc7b52f3a8f3dda985d382f
HC-Gwon <gurckddlz@naver.com>
2023-06-08 23:42:25 +0900

    design: 장바구니 개별 아이템 디자인 구현

--- src/cart/components/WineListItem.tsx
+++ src/cart/components/WineListItem.tsx
@@ -1,87 +1,182 @@
 import Image from 'next/image';
 import styled from '@emotion/styled';
-import { Icon } from '@iconify/react';
+import { Spacing, Text, Divider } from '@boxfoxs/bds-web';
 import { commaizeNumber } from '@boxfoxs/utils';
 
 import { useCartList } from '@/common/hooks/queries/useCartList';
 import { updateAmount, deleteFromCart } from '@/common/api/cart';
 import { CartItem } from '@/common/models';
+import { colors } from '@/common/constants';
 
 export default function WineListItem({
   item,
   index,
+  isSelected,
+  setSelectItem,
 }: {
   item: CartItem;
   index: number;
+  isSelected: boolean;
+  setSelectItem: (bool: boolean) => void;
 }) {
-  const [_, refetch] = useCartList();
+  const [cartList, refetch] = useCartList();
 
-  const addWine = () => {
+  const plusWine = () => {
     updateAmount(item.id, item.amount + 1);
     refetch();
   };
 
-  const deleteWine = () => {
+  const minusWine = () => {
     if (item.amount > 1) {
       updateAmount(item.id, item.amount - 1);
       refetch();
     }
   };
 
-  const clearWine = () => {
+  const deleteWine = () => {
     deleteFromCart(item.id);
     refetch();
   };
 
   return (
-    <ItemWrapper key={`${item.product.name}_${index}`}>
-      <div>
+    <>
+      <Wrapper key={`${item.product.name}_${index}`}>
+        <ContentWrapper>
+          <input
+            id={`${index}`}
+            type="checkbox"
+            checked={isSelected}
+            onChange={() => {
+              setSelectItem(!isSelected);
+            }}
+            style={{ accentColor: colors.primary700Default }}
+          />
+          <Spacing width={12} />
+          <ItemWrapper>
+            <label htmlFor={`${index}`}>
+              <Text size="base" weight="semibold">
+                {item.product.name}
+              </Text>
+            </label>
+            <Spacing height={12} />
+            <WineInfoWrapper>
+              <WineImageWrapper>
+                <WineImageBox>
+                  <Image
+                    src={item.product.image}
+                    alt={`${item.id}`}
+                    width={70}
+                    height={79}
+                  />
+                </WineImageBox>
+                <WineImageBox>
+                  <Image
+                    src={item.product.image}
+                    alt={`${item.id}`}
+                    width={70}
+                    height={79}
+                  />
+                </WineImageBox>
+              </WineImageWrapper>
+              <div>
+                <CountBox>
+                  <Image
+                    src="/images/common/minus_gray.png"
+                    alt="minus"
+                    width={12}
+                    height={12}
+                    onClick={minusWine}
+                  />
+                  <Text size="sm" weight="regular" color={colors.gray900}>
+                    {item.amount}
+                  </Text>
+                  <Image
+                    src="/images/common/plus.png"
+                    alt="plus"
+                    width={12}
+                    height={12}
+                    onClick={plusWine}
+                  />
+                </CountBox>
+                <Text
+                  size="sm"
+                  weight="regular"
+                  color={colors.gray500}
+                  style={{ textDecoration: 'line-through', marginTop: '12px' }}
+                >
+                  {commaizeNumber(item.product.price)}원
+                </Text>
+                <Text
+                  size="lg"
+                  weight="semibold"
+                  color={colors.gray900}
+                  style={{ marginTop: '4px' }}
+                >
+                  {commaizeNumber(item.product.price)}원
+                </Text>
+              </div>
+            </WineInfoWrapper>
+          </ItemWrapper>
+        </ContentWrapper>
         <Image
-          src={item.product.image}
-          alt={`${item.id}`}
-          width={50}
-          height={50}
+          src="/images/common/close.png"
+          alt="close"
+          width={16}
+          height={16}
+          onClick={deleteWine}
+          style={{ marginLeft: '12px' }}
         />
-        <div>{item.product.name}</div>
-        <div>{commaizeNumber(item.product.price)}원</div>
-      </div>
-      <div>
-        <Icon
-          icon="material-symbols:close"
-          width={30}
-          height={30}
-          onClick={clearWine}
+      </Wrapper>
+      {cartList!.length - 1 !== index && (
+        <Divider
+          width="calc(100% - 40px)"
+          color={colors.gray100}
+          style={{ marginLeft: '20px' }}
         />
-        <CountWrapper>
-          <Icon
-            icon="entypo:circle-with-minus"
-            width={30}
-            height={30}
-            onClick={deleteWine}
-          />
-          <div>{item.amount}</div>
-          <Icon
-            icon="entypo:circle-with-plus"
-            width={30}
-            height={30}
-            onClick={addWine}
-          />
-        </CountWrapper>
-      </div>
-    </ItemWrapper>
+      )}
+    </>
   );
 }
 
-const ItemWrapper = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
-  width: 100vw;
-  height: 200px;
-  padding: 0 20px;
-  background-color: pink;
-  border: 1px solid black;
+  align-items: start;
+  padding: 20px;
 `;
 
-const CountWrapper = styled.div`
+const ContentWrapper = styled.div`
   display: flex;
+  align-items: start;
+`;
+
+const ItemWrapper = styled.div``;
+
+const WineInfoWrapper = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+const WineImageWrapper = styled.div`
+  display: flex;
+  gap: 6px;
+`;
+
+const WineImageBox = styled.div`
+  width: 70px;
+  height: 79px;
+  border: 1px solid ${colors.gray300};
+  border-radius: 6px;
+`;
+
+const CountBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 62px;
+  height: 25px;
+  border: 1px solid ${colors.gray300};
+  border-radius: 4px;
+  padding: 6.5px 4px;
 `;
