5db33a1924af32f41e9b9a748247a4626cf230a9
HC-Gwon <gurckddlz@naver.com>
2023-06-09 00:59:10 +0900

    design: 와인 리스트 부분 기본적인 디자인 구현

--- src/cart/pages/wineList.tsx
+++ src/cart/pages/wineList.tsx
@@ -1,47 +1,106 @@
-import { useMemo } from 'react';
-import { useRouter } from 'next/router';
+import { useState, useMemo, useEffect } from 'react';
+import Image from 'next/image';
 import styled from '@emotion/styled';
-import { commaizeNumber } from '@boxfoxs/utils';
-import { sum } from 'lodash';
+import { Divider, Text } from '@boxfoxs/bds-web';
 
 import WineListItem from '@cart/components/WineListItem';
 import { CartItem } from '@/common/models';
+import { colors } from '@/common/constants';
 
-export default function WineList({ wineList }: { wineList: CartItem[] }) {
-  const router = useRouter();
-
-  const totalPrice = useMemo(
-    () => sum(wineList.map((item) => item.amount * item.product.price)),
-    [wineList]
+export default function WineList({
+  wineList,
+  isItemSelected,
+}: {
+  wineList: CartItem[];
+  isItemSelected: (bool: boolean) => void;
+}) {
+  const [selectedItems, setSelectedItems] = useState<boolean[]>(
+    (Array(wineList.length) as boolean[]).fill(false)
   );
+
+  const isSelectedAll = useMemo(() => {
+    const selectedItemLen = selectedItems.filter(
+      (isSelected: boolean) => isSelected
+    ).length;
+
+    if (selectedItemLen === wineList.length) return true;
+    return false;
+  }, [selectedItems]);
+
+  useEffect(() => {
+    if (selectedItems.filter((item) => item === true).length > 0)
+      isItemSelected(true);
+    else isItemSelected(false);
+  }, [selectedItems]);
 
   const renderWineList = () => {
     return wineList.map((item: CartItem, index: number) => {
-      return <WineListItem item={item} index={index} />;
+      return (
+        <WineListItem
+          key={`${index}_${item.product.name}`}
+          item={item}
+          index={index}
+          isSelected={selectedItems[index]}
+          setSelectItem={(bool: boolean) => {
+            setSelectedItems((prev: boolean[]) => {
+              const newList = [...prev];
+              newList[index] = bool;
+              return newList;
+            });
+          }}
+        />
+      );
     });
   };
 
   return (
-    <>
+    <Wrapper>
+      <SelectAllWrapper>
+        <input
+          type="checkbox"
+          checked={isSelectedAll}
+          onChange={() => {
+            setSelectedItems(
+              (Array(wineList.length) as boolean[]).fill(!isSelectedAll)
+            );
+          }}
+          style={{ accentColor: colors.primary700Default }}
+        />
+        <Text size="base" weight="regular">
+          전체 선택
+        </Text>
+      </SelectAllWrapper>
+      <Divider width="100%" color={colors.gray100} />
       {renderWineList()}
-      <BottomNavigator
-        onClick={() => {
-          router.push('/OrderPage');
-        }}
-      >
-        {commaizeNumber(totalPrice)}원 주문하러가기
-      </BottomNavigator>
-    </>
+      <QuestionText>
+        <Image
+          src="/images/common/question_gray.png"
+          alt="minus"
+          width={12}
+          height={12}
+        />
+        <Text size="sm" weight="regular" color={colors.gray500}>
+          와인에 스낵은 왜 항상 포함되어 있나요?
+        </Text>
+      </QuestionText>
+    </Wrapper>
   );
 }
 
-const BottomNavigator = styled.div`
-  position: fixed;
-  bottom: 0;
+const Wrapper = styled.div``;
+
+const SelectAllWrapper = styled.label`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 20px 0;
+  padding: 0 20px;
+  gap: 8px;
+`;
+
+const QuestionText = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100px;
-  background-color: green;
+  gap: 4px;
 `;
