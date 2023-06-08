9a58cec08d9a787f99e010fc01b8a2d615e58b64
HC-Gwon <gurckddlz@naver.com>
2023-06-09 01:02:46 +0900

    design: 장바구니 페이지 기본적인 디자인 구현

--- src/cart/pages/index.tsx
+++ src/cart/pages/index.tsx
@@ -1,65 +1,78 @@
+import { useState, useMemo } from 'react';
 import { useRouter } from 'next/router';
 import styled from '@emotion/styled';
-import { Icon } from '@iconify/react';
+import { Spacing, Divider, Text } from '@boxfoxs/bds-web';
+import { sum } from 'lodash';
 
 import WineList from '@cart/pages/wineList';
 import NoWineList from '@cart/pages/noWineList';
+import Payment from '@cart/pages/payment';
+import { AppBar } from '@/common/components';
 import { useCartList } from '@/common/hooks/queries/useCartList';
-import { clearCart } from '@/common/api/cart';
+import { colors } from '@/common/constants';
 
 export default function CartPage() {
   const router = useRouter();
   const [cartList, refetch] = useCartList();
+  const [hasSelectedItem, setHasSelectedItem] = useState(false);
+
+  const totalPrice = useMemo(() => {
+    if (cartList && cartList.length > 0)
+      return sum(cartList.map((item) => item.amount * item.product.price));
+    return 0;
+  }, [cartList]);
+
+  const buttonText = useMemo(() => {
+    if (hasSelectedItem) return '323,400원 주문하기';
+    return '상품을 선택해주세요';
+  }, [hasSelectedItem]);
 
   return (
     <Container>
-      <TopNavigator>
-        <Wrapper>
-          <Icon
-            icon="material-symbols:chevron-left-rounded"
-            width={30}
-            height={30}
-            onClick={() => router.back()}
-          />
-          <div>장바구니</div>
-        </Wrapper>
-        <Icon
-          icon="ph:trash"
-          width={30}
-          height={30}
-          onClick={() => {
-            clearCart();
-            refetch();
-          }}
-        />
-      </TopNavigator>
+      <AppBar back>
+        <AppBar.Title>장바구니</AppBar.Title>
+      </AppBar>
       {cartList && cartList.length ? (
-        <WineList wineList={cartList} />
+        <WineList wineList={cartList} isItemSelected={setHasSelectedItem} />
       ) : (
         <NoWineList />
       )}
+      <Spacing height={20} />
+      <Divider width="100%" height="6px" color={colors.gray100} />
+      <Payment />
+      <WarningText>
+        <Text size="base" weight="regular" color={colors.gray600}>
+          (주)마이너리는 통신판매중개자이며, 통신판매의 당사자가 아닙니다.
+          따라서(주)마이너리는 상품, 거래정보 및 거래에 대하여 책임을 지지
+          않습니다.
+        </Text>
+      </WarningText>
+      <OrderButton isItemSelected={hasSelectedItem}>
+        <Text size="xl" weight="semibold" color={colors.defaultWhite}>
+          {buttonText}
+        </Text>
+      </OrderButton>
     </Container>
   );
 }
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
-  background-color: red;
-  padding-bottom: 100px;
 `;
 
-const TopNavigator = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 56px;
-  padding: 0 20px;
+const WarningText = styled.div`
+  padding: 20px;
+  background-color: ${colors.gray200};
 `;
 
-const Wrapper = styled.div`
+const OrderButton = styled.div<{ isItemSelected: boolean }>`
   display: flex;
+  justify-content: center;
   align-items: center;
+  padding: 16px 0;
+  background-color: ${({ isItemSelected }) =>
+    isItemSelected ? colors.primary700Default : colors.gray400};
 `;
