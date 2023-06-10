246895d166cd9d15070b4618e885698bad22b67f
Ryan <114748930+RyanKw0n@users.noreply.github.com>
2023-06-10 15:30:25 +0900

    Ryan/home (#23)
    * feat: Home 화면 추가
    
    * refactor: Home 화면 리팩토링(1)
    
    * refactor: WineCard 컴포넌트 리팩토링
    
    * refactor: Home 화면 리팩토링(2) 및 일부 수정
    
    * feat: Home 화면에 footer 추가
    
    ---------
    
    Co-authored-by: 김성래 - Boxfox <boxfoxsg619@gmail.com>

---
+++ src/common/components/footer/Footer.tsx
@@ -0,0 +1,58 @@
+import { css } from '@emotion/react';
+import Image from 'next/image';
+import { Text } from '@boxfoxs/bds-web';
+import { colors } from '@/common/constants';
+import logo from '../../../../public/assets/image_logo.svg';
+import { Space } from '../space';
+
+export function Footer() {
+  return (
+    <section
+      css={css`
+        width: 100%;
+        background-color: ${colors.gray200};
+      `}
+    >
+      <div
+        css={css`
+          padding: 36px 20px;
+        `}
+      >
+        <Image src={logo} width={30} height={18} alt="logo" />
+        <Space bottom="20px" />
+        <div
+          css={css`
+            display: flex;
+          `}
+        >
+          <Text size="base" weight="semibold" color="gray900">
+            문의하기
+          </Text>
+          {/* FIXME: seperator 임시로 대응 */}
+          <Space left="5px" />
+          |
+          <Space right="5px" />
+          <Text size="base" weight="semibold" color="gray900">
+            이용약관
+          </Text>
+          <Space left="5px" />
+          |
+          <Space right="5px" />
+          <Text size="base" weight="semibold" color="gray900">
+            개인정보처리방침
+          </Text>
+        </div>
+        <Space bottom="20px" />
+        <div
+          css={css`
+            white-space: pre-wrap;
+          `}
+        >
+          <Text size="sm" weight="regular" color={colors.gray600}>
+            {`대표 : \n사업자등록번호 :\n고객센터 : 서울특별시 건대...?\n\nⓒ 2023 Minery. All rights reserved.`}
+          </Text>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,5 +1,6 @@
 export * from './buttons';
 export * from './logging';
 export * from './appbar';
+export * from './space';
 export * from './layout';
 export * from './form';
