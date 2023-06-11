96981c630e3d3950d4ebc4f86b5824e93790fc55
HC-Gwon <45168134+HC-Gwon@users.noreply.github.com>
2023-06-11 16:08:41 +0900

    design: 결제하기 페이지 디자인 구현 (#26)
    * design: 와인 리스트 부분 디자인 적용
    
    * design: 구매자 정보 부분 디자인 구현
    
    * design: 결제정보 부분 디자인 구현
    
    * design: 배달상품 주의사항 부분 디자인 구현

---
+++ src/order/containers/UserInfo.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Text, Divider } from '@boxfoxs/bds-web';
+import styled from '@emotion/styled';
+
+import { useUserQuery } from '@/common/hooks/queries';
+import { colors } from '@/common/constants';
+
+export default function UserInfo() {
+  const [userInfo] = useUserQuery(0); // TODO userId 변경
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <>
+      <Wrapper>
+        <Text size="lg" weight="semibold" color={colors.gray900}>
+          구매자 정보
+        </Text>
+        <InfoWrapper>
+          <UserInfoWrapper>
+            <Text size="base" weight="medium" color={colors.gray900}>
+              권혁창
+            </Text>
+            <Text size="base" weight="medium" color={colors.gray900}>
+              010 - 9667 - 5855
+            </Text>
+          </UserInfoWrapper>
+          <DirectlyReceving htmlFor="check">
+            <input
+              id="check"
+              type="checkbox"
+              checked={isChecked}
+              onChange={() => {
+                setIsChecked(!isChecked);
+              }}
+              style={{ accentColor: colors.primary700Default }}
+            />
+            <Text size="base" weight="medium" color={colors.gray900}>
+              본인이 수령해요
+            </Text>
+          </DirectlyReceving>
+        </InfoWrapper>
+      </Wrapper>
+      <Divider width="100%" height={6} color={colors.gray100} />
+    </>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 24px 20px;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const UserInfoWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+const DirectlyReceving = styled.label`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 8px;
+`;
