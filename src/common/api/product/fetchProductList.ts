1aae98982d7e4c042ae07e42770a93794708901b
boxfox619 <boxfoxsg619@gmail.com>
2023-06-11 17:51:26 +0900

    메인 레이아웃 모바일로 적용, fetchProductList api 수정

--- src/common/api/product/fetchProductList.ts
+++ src/common/api/product/fetchProductList.ts
@@ -1,7 +1,21 @@
 import { Product } from '@/common/models/Product';
 import { requester } from '@/common/api/requester';
+import { WineType } from '@/common/models';
 
-export async function fetchProductList() {
-  const res = await requester.get<Product[]>('/product');
+export async function fetchProductList({
+  wineType,
+  order,
+}: {
+  wineType?: WineType;
+  order?: OrderType;
+}) {
+  const res = await requester.get<Product[]>('/product', {
+    params: { wineType, order },
+  });
   return res.data;
+}
+
+export enum OrderType {
+  POPULAR,
+  LOWEST_PRICE,
 }
