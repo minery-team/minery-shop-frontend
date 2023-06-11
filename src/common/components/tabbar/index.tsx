0a299f0d62d8b2a080c9789992073150f184c709
graykick <jisoo328@gmail.com>
2023-06-11 18:45:00 +0900

    :sparkles: 상품설명 Section 구현

---
+++ src/common/components/tabbar/index.tsx
@@ -0,0 +1,81 @@
+import { colors } from '@/common/constants';
+import { Flex, inDesktop } from '@boxfoxs/bds-web';
+import styled from '@emotion/styled';
+import React, { ComponentProps } from 'react';
+
+export function TabBar({
+  data,
+  value,
+  onClick,
+  ...props
+}: {
+  data: { name: string; id: string }[];
+  value: string;
+  onClick: (id: string) => void;
+} & Omit<ComponentProps<typeof Container>, 'onClick'>) {
+  return (
+    <Container {...props}>
+      {data.map((item) => (
+        <TabBarButton
+          key={item.id}
+          active={value === item.id}
+          onClick={() => onClick(item.id)}
+        >
+          {item.name}
+        </TabBarButton>
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled(Flex.CenterVertical)``;
+
+const TabBarButton = styled.button<{ active?: boolean }>`
+  background: none;
+  border: none;
+  font-size: 16px;
+  line-height: 19px;
+  font-weight: ${(p) => (p.active ? '700' : '400')};
+  padding: 10px 0;
+  flex: 1;
+  color: ${(p) => (p.active ? colors.gray900 : colors.gray300)};
+  border-bottom: ${(p) =>
+    p.active ? `2px solid ${colors.gray900}` : `1px solid ${colors.gray900}`};
+  ${(p) => (p.active ? '' : pressableStyle.opacity())}
+`;
+
+const pressableStyle = {
+  background: (background = colors.gray100) => `
+    transition: background 250ms;
+    &:active {
+      background: ${background};
+    }
+    ${inDesktop(`
+      &:focus {
+        background: ${background};
+      }
+    `)}
+  `,
+  opacity: (opacity = 0.7) => `
+    transition: opacity 200ms;
+    &:active {
+      opacity: ${opacity};
+    }
+    ${inDesktop(`
+      &:focus {
+        opacity: ${opacity};
+      }
+    `)}
+  `,
+  scale: (scale = 0.97) => `
+    transition: transform 150ms;
+    &:active {
+      transform: scale(${scale});
+    }
+    ${inDesktop(`
+      &:focus {
+        transform: scale(${scale});
+      }
+    `)}
+  `,
+};
--- src/product/pages/index.tsx
+++ src/product/pages/index.tsx
@@ -1,29 +1,59 @@
-import { AppBar } from '@/common/components';
+import { AppBar, Section, Space } from '@/common/components';
 import { Carousel } from '@/common/components/carousel';
-import Container from '@/common/components/layout/Container';
+import { Container } from '@/common/components/layout/Container';
 import { colors } from '@/common/constants';
 import { useProductList } from '@/common/hooks/queries/useWineList';
-import { Product } from '@/common/models';
+import {
+  FLAG_BY_COUNTRY,
+  NAME_BY_WINE_TYPE,
+  Product,
+  getWineFlagLabel,
+} from '@/common/models';
+import { Spacing, Text } from '@boxfoxs/bds-web';
 import { Path } from '@boxfoxs/next';
+import { commaizeNumber } from '@boxfoxs/utils';
 import styled from '@emotion/styled';
 import Image from 'next/image';
+import { TabBar } from '@/common/components/tabbar';
+import { useRef, useState } from 'react';
+import notice from '../../../public/assets/notice.svg';
+import snack_for_wine from '../../../public/assets/snack_for_wine.png';
+import package_for_wine from '../../../public/assets/package_for_wine.png';
+import DescriptionSection from '../component/DescriptionSection';
+
+const HEADER_SIZE = 112;
+
+type TabTitle = 'description' | 'review' | 'label';
 
 const ProductPage = () => {
   const id = Number(Path.get('id') || 5);
   const [products] = useProductList();
   // const product = products?.find((product) => product.id === id);
   const product: Product = products?.[0];
+  const containerRef = useRef<HTMLElement>(null);
+  const sectionRefs = useRef<Record<string, HTMLElement | null>>({});
+  const [tab, setTab] = useState<TabTitle>('description');
+  const handleTabClick = (selectedTab: TabTitle) => {
+    const root = containerRef.current;
+    const target = sectionRefs.current[selectedTab];
+    if (!target || !root) {
+      return;
+    }
+    const rect = target.getBoundingClientRect();
+    containerRef.current?.scrollTo({
+      top: root.scrollTop + rect.top - HEADER_SIZE + 8,
+      behavior: 'smooth',
+    });
+  };
 
   if (!product) return <div>해당 상품이 없습니다 :(</div>;
 
-  console.log(products);
-
   return (
-    <Container>
+    <Container ref={containerRef}>
       <AppBar back />
       <Carousel
         dots
-        dotsClass="df"
+        dotsClass=""
         // eslint-disable-next-line react/no-unstable-nested-components
         appendDots={(dots) => {
           return <CarouselDotsWrapper>{dots}</CarouselDotsWrapper>;
@@ -35,6 +65,141 @@ const ProductPage = () => {
           </Rectangle>
         ))}
       </Carousel>
+
+      <Section>
+        {/* 국가 및 와인 타입 */}
+        <div style={{ display: 'flex', gap: '6px', color: colors.gray700 }}>
+          {getWineFlagLabel(product.wine.country) ? (
+            <span>{getWineFlagLabel(product.wine.country)}</span>
+          ) : null}
+          <Text>{product.wine.country}</Text>
+          <span>|</span>
+          <Text>{NAME_BY_WINE_TYPE[product.wine.type]}</Text>
+        </div>
+        {/* 타이틀 */}
+        <Spacing height={12} />
+        <Text size="xl" weight="semibold">
+          {product.name}
+        </Text>
+        <Text size="base" weight="regular" color={colors.gray600}>
+          {product.enName}
+        </Text>
+
+        {/* 가격 */}
+        <Spacing height={12} />
+        <div style={{ display: 'flex', gap: '8px', alignItems: 'flex-end' }}>
+          <Text
+            color={colors.primary700Default}
+            size="heading4"
+            weight="semibold"
+          >
+            {/* TODO: 할인율 소숫점 정책 필요 */}
+            {((product.originalPrice - product.price) / product.originalPrice) *
+              100}
+            %
+          </Text>
+          <Text size="heading4" weight="semibold">
+            {commaizeNumber(product.price)}원
+          </Text>
+          <Text
+            style={{
+              textDecoration: 'line-through',
+            }}
+            color={colors.gray500}
+          >
+            {commaizeNumber(product.originalPrice)}원
+          </Text>
+        </div>
+      </Section>
+      {/* 배송비 */}
+      <div
+        style={{
+          background: colors.gray100,
+          padding: '20px',
+          display: 'flex',
+          alignItems: 'center',
+          gap: '8px',
+        }}
+      >
+        <Image src={notice} width={16} height={16} alt="배송비 안내" />
+        <Text>30,000원 이상 무료배송</Text>
+      </div>
+      <Section>
+        {/* 상품 설명 */}
+        <Text size="xl" weight="semibold">
+          상품구성
+        </Text>
+        <Spacing height={20} />
+        <div style={{ display: 'flex', gap: '16px' }}>
+          <div>
+            <img
+              style={{
+                width: '106px',
+                height: '120px',
+                borderRadius: '6px',
+                background: colors.gray200,
+              }}
+              src={product.image}
+            />
+            <Spacing height={8} />
+            <Text size="base" weight="semibold">
+              마이너리 추천 와인
+            </Text>
+            <Spacing height={4} />
+            <Text size="sm" weight="regular">
+              1병
+            </Text>
+          </div>
+          <div>
+            <Image
+              style={{
+                width: '106px',
+                height: '120px',
+                borderRadius: '6px',
+                background: colors.gray200,
+              }}
+              src={snack_for_wine}
+            />
+            <Spacing height={8} />
+            <Text size="base" weight="semibold">
+              와인과 잘 어울리는 스낵 세트
+            </Text>
+            <Spacing height={4} />
+            <Text size="sm" weight="regular">
+              1개
+            </Text>
+          </div>
+          <div>
+            <Image
+              style={{
+                width: '106px',
+                height: '120px',
+                borderRadius: '6px',
+                background: colors.gray200,
+              }}
+              src={package_for_wine}
+            />
+            <Spacing height={8} />
+            <Text size="base" weight="semibold">
+              집에서 받아보는 안심포장 패키지
+            </Text>
+            <Spacing height={4} />
+            <Text size="sm" weight="regular">
+              1개
+            </Text>
+          </div>
+        </div>
+      </Section>
+      <StyledTabBar
+        value={tab}
+        data={[
+          { name: '상품 소개', id: 'description' },
+          { name: '리뷰', id: 'review' },
+          { name: '라벨 상세', id: 'label' },
+        ]}
+        onClick={handleTabClick}
+      />
+      <DescriptionSection product={product} />
     </Container>
   );
 };
@@ -44,10 +209,12 @@ export default ProductPage;
 const CarouselDotsWrapper = styled.ul`
   position: absolute;
   bottom: 0px;
-  display: block;
+  display: flex;
   width: 100%;
   padding: 20px;
   margin: 0;
+  text-align: left;
+  gap: 8px;
 
   li {
     display: inline-block;
@@ -87,4 +254,12 @@ const Rectangle = styled.div`
     object-fit: contain;
     object-position: center;
   }
+`;
+
+const StyledTabBar = styled(TabBar)`
+  background: white;
+  position: sticky;
+  top: 50px;
+  z-index: 2;
+  padding-top: 4px;
 `;
