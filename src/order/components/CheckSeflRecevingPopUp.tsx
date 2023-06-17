27d82ab1666d67dc389cd9a59c10ea5893f3d16a
HC-Gwon <gurckddlz@naver.com>
2023-06-17 11:59:55 +0900

    feat: 배송지 정보 등록/본인 수령 체크 관련 팝업 구현

---
+++ src/order/components/CheckSeflRecevingPopUp.tsx
@@ -0,0 +1,51 @@
+import { Text, usePopup } from '@boxfoxs/bds-web';
+import styled from '@emotion/styled';
+
+import { colors } from 'common/constants';
+import { PopUp } from 'common/components';
+import { useCallback } from 'react';
+
+export function CheckSelfRecevingPopUp({ onClose }: { onClose: () => void }) {
+  return (
+    <PopUp
+      type="confirm"
+      content={
+        <TextWrapper>
+          <StyledText size="lg" weight="semibold" color={colors.gray900}>
+            <div>본인 수령 여부를 체크해주세요</div>
+          </StyledText>
+        </TextWrapper>
+      }
+      confirmText="확인"
+      onConfirm={onClose}
+    />
+  );
+}
+
+export function useCheckSelfReceving() {
+  const { open, close } = usePopup('check-self-receving-popup');
+
+  return useCallback(() => {
+    open({
+      children: <CheckSelfRecevingPopUp onClose={close} />,
+      onClose: close,
+    });
+  }, [open, close]);
+}
+
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .highlight {
+    color: ${colors.primary700Default};
+  }
+`;
+
+const StyledText = styled(Text)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
