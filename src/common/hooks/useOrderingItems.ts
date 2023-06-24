068e79bed7db23483afc5527b9ca4625c943f4a8
boxfox619 <boxfoxsg619@gmail.com>
2023-06-24 15:29:45 +0900

    fix

--- src/common/hooks/useOrderingItems.ts
+++ src/common/hooks/useOrderingItems.ts
@@ -1,12 +1,37 @@
+import { QS } from '@boxfoxs/next';
 import { CartItem } from 'common/models';
 import { orderItems } from 'common/recoil/orderItems';
+import { useMemo } from 'react';
 import { UseQueryOptions } from 'react-query';
 import { useRecoilValue } from 'recoil';
+import { useProductList } from './queries/useWineList';
 import { useCart } from './useCart';
 
 export function useOrderingItems(options?: UseQueryOptions<CartItem[]>) {
+  const [products] = useProductList();
+  const directOrderItem = useMemo<CartItem | null>(() => {
+    const data = {
+      id: Number(QS.get('productId')),
+      amount: Number(QS.get('orderAmount')),
+    };
+    const product = products?.find((p) => p.id === data.id);
+    if (!data.id || !data.amount || !product) {
+      return null;
+    }
+    return {
+      id: product.id,
+      isDirect: true,
+      amount: data.amount,
+      product,
+      options: [],
+    };
+  }, [products]);
   const list = useRecoilValue(orderItems);
   const cart = useCart(options);
+
+  if (directOrderItem) {
+    return [directOrderItem];
+  }
 
   return list.length ? list : cart.value;
 }
