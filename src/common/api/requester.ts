172a943909de0362f89df5f8bd282dbf0da7ccab
boxfox619 <boxfoxsg619@gmail.com>
2023-07-22 20:32:02 +0900

    fix

--- src/common/api/requester.ts
+++ src/common/api/requester.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { interceptAuthToken } from './interceptAuthToken';
 
 export const requester = axios.create({
-  // baseURL: 'https://r0up0kec01.execute-api.ap-northeast-2.amazonaws.com/live',
-  baseURL: 'http://localhost:3000/live',
+  baseURL: 'https://r0up0kec01.execute-api.ap-northeast-2.amazonaws.com/live',
+  // baseURL: 'http://localhost:3000/live',
 });
 
 requester.interceptors.request.use(interceptAuthToken);
