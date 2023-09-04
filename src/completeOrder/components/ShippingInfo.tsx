b3c17ae1d17221ceee07d47b8a6cbb61ff0a6c50
RyanKw0n <alwayskyu2920@gmail.com>
2023-09-04 09:02:00 +0900

    fix: 문구수정 누락사항 추가

--- src/completeOrder/components/ShippingInfo.tsx
+++ src/completeOrder/components/ShippingInfo.tsx
@@ -17,15 +17,15 @@ export function ShippingInfo({
   return (
     <Wrapper>
       <Text size="lg" weight="semibold" color={colors.gray900}>
-        배송지 정보
+        픽업지 정보
       </Text>
       <Divider width="100%" height={1} color={colors.gray100} />
       <TextWrapper>
         <Text size="base" weight="semibold" color={colors.gray900}>
-          받는 분
+          픽업지
         </Text>
         <Text size="base" weight="medium" color={colors.gray900}>
-          {userInfo?.name}
+          {/* {userInfo?.name} */}A 와인마트
         </Text>
       </TextWrapper>
       <TextWrapper>
@@ -33,7 +33,8 @@ export function ShippingInfo({
           연락처
         </Text>
         <Text size="base" weight="medium" color={colors.gray900}>
-          {formatPhoneNumber(userInfo?.phone ?? '')}
+          {/* {formatPhoneNumber(userInfo?.phone ?? '')} */}
+          010-6423-3522
         </Text>
       </TextWrapper>
       <TextWrapper>
@@ -41,7 +42,8 @@ export function ShippingInfo({
           주소
         </Text>
         <Text size="base" weight="medium" color={colors.gray900}>
-          {`${defaultAddress?.address}(${defaultAddress?.detailAddress})`}
+          {/* {`${defaultAddress?.address}(${defaultAddress?.detailAddress})`} */}
+          화양동 46-30번지 401호
         </Text>
       </TextWrapper>
       {deliveryMessage && (
